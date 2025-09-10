0a7207146265d7a067ff6b35a50f6741f5136520
lavishgupta613-oss <lavishgupta613@gmail.com>
2025-09-11 01:32:58 +0530

    Initial commit with dependencies setup

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.0.1",
     author="Lavish Gupta",
     author_email="lavishgupta613@gmail.com",
-    install_requires=["gemini","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["google-generativeai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages(),
 )
